fd064bfabf22c169fb5ea5c36f66b828fff5b25c
HotG8119 <k508060@gmail.com>
2023-07-29 02:20:29 +0800

    feat: create index.hbs and index route

--- app.js
+++ app.js
@@ -5,13 +5,26 @@ const bcrypt = require("bcryptjs");
 const app = express();
 const PORT = 3000;
 
+const db = require("./models");
+const Todo = db.Todo;
+const User = db.User;
+
 app.engine("hbs", exphbs({ defaultLayout: "main", extname: ".hbs" }));
 app.set("view engine", "hbs");
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
 
 app.get("/", (req, res) => {
-  res.send("hello world");
+  return Todo.findAll({
+    raw: true,
+    nest: true,
+  })
+    .then(todos => {
+      return res.render("index", { todos: todos });
+    })
+    .catch(error => {
+      return res.status(422).json(error);
+    });
 });
 
 app.get("/users/login", (req, res) => {
@@ -27,7 +40,10 @@ app.get("/users/register", (req, res) => {
 });
 
 app.post("/users/register", (req, res) => {
-  res.send("register");
+  const { name, email, password, confirmPassword } = req.body;
+  User.create({ name, email, password }).then(user => {
+    res.redirect("/");
+  });
 });
 
 app.get("/users/logout", (req, res) => {
