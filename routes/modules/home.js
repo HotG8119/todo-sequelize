24f078f5b0232d563bd03226112763fe69069fab
HotG8119 <k508060@gmail.com>
2023-07-30 00:14:20 +0800

    Fix: sequelize syntax in todo.js and home.js

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -3,18 +3,22 @@ const router = express.Router();
 
 const db = require("../../models");
 const Todo = db.Todo;
+const User = db.User;
 
 router.get("/", (req, res) => {
-  return Todo.findAll({
-    raw: true,
-    nest: true,
-  })
-    .then(todos => {
-      return res.render("index", { todos });
+  User.findByPk(req.user.id).then(() => {
+    return Todo.findAll({
+      raw: true,
+      nest: true,
+      where: { UserId: req.user.id },
     })
-    .catch(error => {
-      return res.status(422).json(error);
-    });
+      .then(todos => {
+        return res.render("index", { todos });
+      })
+      .catch(error => {
+        return res.status(422).json(error);
+      });
+  });
 });
 
 module.exports = router;
--- routes/modules/todos.js
+++ routes/modules/todos.js
@@ -9,32 +9,37 @@ router.get("/new", (req, res) => {
 });
 
 router.post("/", (req, res) => {
+  const UserId = req.user.id;
   const name = req.body.name;
 
-  return Todo.create({ name })
+  return Todo.create({ name, UserId })
     .then(() => res.redirect("/"))
     .catch(error => console.log(error));
 });
 
 router.get("/:id", (req, res) => {
+  const UserId = req.user.id;
   const id = req.params.id;
-  return Todo.findByByPk(id)
+  return Todo.findOne({ where: { id, UserId } })
     .then(todo => res.render("detail", { todo: todo.toJSON() }))
     .catch(error => console.log(error));
 });
 
 router.get("/:id/edit", (req, res) => {
+  const UserId = req.user.id;
   const id = req.params.id;
-  return Todo.findByByPk(id)
-    .then(todo => res.render("edit", { todo: todo.toJSON() }))
+
+  return Todo.findOne({ where: { id, UserId } })
+    .then(todo => res.render("edit", { todo: todo.get() }))
     .catch(error => console.log(error));
 });
 
 router.put("/:id", (req, res) => {
+  const UserId = req.user.id;
   const id = req.params.id;
   const { name, isDone } = req.body;
 
-  return Todo.findByPk(id)
+  return Todo.findOne({ where: { id, UserId } })
     .then(todo => {
       todo.name = name;
       todo.isDone = isDone === "on";
@@ -45,9 +50,10 @@ router.put("/:id", (req, res) => {
 });
 
 router.delete("/:id", (req, res) => {
+  const UserId = req.user.id;
   const id = req.params.id;
-  return Todo.findById(id)
-    .then(todo => todo.remove())
+  return Todo.findOne({ where: { id, UserId } })
+    .then(todo => todo.destroy())
     .then(() => res.redirect("/"))
     .catch(error => console.log(error));
 });
